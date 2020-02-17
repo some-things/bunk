ae787a76141871535ddedba767ec5abb4a78a3cf
Dustin Nemes <dustinmnemes@gmail.com>
2020-02-17 05:03:15 -0600

    Add basic up logic

--- cmd/bunk/cmd/up.go
+++ cmd/bunk/cmd/up.go
@@ -16,10 +16,43 @@ limitations under the License.
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
+	"sigs.k8s.io/yaml"
 )
+
+// KubernetesItems : Structure of Kubernetes yaml files -- make this more clear/redefine
+type KubernetesItems struct {
+	Items []interface{} `yaml:"items"`
+}
+
+// KubernetesObject : Generic minimal structure for Kubernetes objects
+type KubernetesObject struct {
+	APIVersion string `yaml:"apiVersion"`
+	Kind       string `yaml:"kind"`
+	Metadata   struct {
+		Name      string `yaml:"name"`
+		Namespace string `yaml:"namespace,omitempty"`
+	} `yaml:"metadata"`
+}
+
+// DatabaseObject : Structure of each item in the k3d SQLite3 backend database
+type DatabaseObject struct {
+	id             int
+	name           string
+	created        int
+	deleted        int
+	createRevision int
+	prevRevision   int
+	lease          int
+	value          string
+	oldValue       string
+}
 
 // upCmd represents the up command
 var upCmd = &cobra.Command{
@@ -33,6 +66,50 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("up called")
+
+		f, err := os.OpenFile("/Users/dn/Documents/logs/tickets/beta-test/bundle-20200217T015050/cluster-data/api-resources/nodes.yaml", os.O_RDONLY, 0444)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer f.Close()
+
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var kubernetesItems KubernetesItems
+		err = yaml.Unmarshal(content, &kubernetesItems)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for i := 0; i < len(kubernetesItems.Items); i++ {
+			objectJSON, err := json.Marshal(kubernetesItems.Items[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fullObjectJSON := objectJSON
+
+			var kubernetesObject KubernetesObject
+
+			err = json.Unmarshal(objectJSON, &kubernetesObject)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			name := kubernetesObject.Metadata.Name
+			fmt.Println("Name: ", name)
+			if kubernetesObject.Metadata.Namespace != "" {
+				namespace := kubernetesObject.Metadata.Namespace
+				fmt.Println("Namespace: ", namespace)
+			}
+
+			fmt.Printf("%s\n\n", fullObjectJSON)
+		}
 	},
 }
 
